c4a5a821dbbde33777e1fa327b251682fcb325f9
tomito26 <tommybwah@gmail.com>
2020-10-18 19:28:11 +0300

    added models for  the image and profile

--- gram/models.py
+++ gram/models.py
@@ -4,3 +4,22 @@ from cloudinary.models import CloudinaryField
 # Create your models here.
 class Image(models.Model):
     image = CloudinaryField('image')
+    image_name= models.CharField(max_length=60)
+    caption = models.TextField()
+    profile=models.ForeignKey('Profile',on_delete=models.CASCADE)
+    likes = models.IntegerField(default=0)
+    comments = models.CharField(max_length=3000)
+    
+    def __str__(self):
+        return self.image_name
+    
+    def save_image(self):
+        self.save()
+        
+    def delete_image(self):
+        self.delete()
+    
+class Profile(models.Model):
+    profile_photo =CloudinaryField('image')
+    Bio=models.TextField()
+    
