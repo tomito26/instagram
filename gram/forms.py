98725f8059084efcb559fcc7590ffec3123c9892
tomito26 <tommybwah@gmail.com>
2020-10-19 03:15:19 +0300

    added the profile form and Imageform on form.py

--- gram/forms.py
+++ gram/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-
+from .models import Image,Profile
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
@@ -9,3 +9,14 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+class ImageForm(forms.ModelForm):
+    class Meta:
+        model = Image
+        fields = ['image','image_name','caption','likes','comments']
+        
+    
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = '__all__'
